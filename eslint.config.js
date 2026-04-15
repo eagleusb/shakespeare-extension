b787b05988eab9947eeda265f67450a477393b33
eagleusb <git@eagleusb.com>
2026-04-16 01:56:10 +0200

    feat(eslint) add eslint-plugin-no-unsanitized

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import tseslint from "typescript-eslint";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import noUnsanitized from "eslint-plugin-no-unsanitized";
 
 export default defineConfig(
   {
     ignores: ["dist/**", "node_modules/**"],
   },
   ...tseslint.configs.recommended,
+  noUnsanitized.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2024,
